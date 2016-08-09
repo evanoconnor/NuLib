35e7ba32f3fe5fcfcb9b25db70d106e27eccf0f7
Chris Sullivan <sullivan@nscl.msu.edu>
2016-08-09 15:00:15 -0400

    With debian jessie, some changes were needed to the compilation. Now a setup_python.sh script should be sourced before compiling and running to ensure the LD_LIBRARY_PATH is setup to point to the needed HDF5 libraries and the NuLib shared library.

--- src/pyinterface.F90
+++ src/pyinterface.F90
@@ -7,6 +7,80 @@ module pynulib
 
   contains
 
+
+    subroutine standard_nulib_init
+      use nuclei_hempel
+      use inputparser
+      use nulib
+      implicit none
+
+      !many people use different number of species, this is to denote how they are devided up.
+      ! mytable_neutrino_scheme = 1 (three output species)
+      ! species #1: electron neutrino             #2 electron antineutrino
+      !         #3: muon+tau neutrino+antineutrino
+      ! neutrino_scheme = 2 (four output species)
+      ! species #1: electron neutrino             #2 electron antineutrino
+      !         #3: muon+tau neutrino             #4 mu and tau antineutrino
+      ! neutrino_scheme = 3 (six output species)
+      ! species #1: electron neutrino             #2 electron antineutrino
+      !         #3: muon neutrino                 #4 mu antineutrino
+      !         #5: tau neutrino                  #6 tau antineutrino
+      integer :: mytable_neutrino_scheme = 1
+
+      !number of species for nulib to calculation interactions for, must
+      !be six currently, average is done via above parameter
+      integer :: mytable_number_species = 6
+
+      !number of energy groups
+      integer :: mytable_number_groups = 18
+
+      !NuLib parameters file (weak rates and EOS)
+      character*200 :: parameters_filename = "./parameters"
+      real*8 dxfac,mindx
+      integer :: i
+
+      call input_parser(parameters_filename)
+      !this sets up many coefficients and creates the energy grid (one
+      !zone + log spacing) see nulib.F90:initialize_nulib
+      call initialize_nulib(mytable_neutrino_scheme,mytable_number_species,mytable_number_groups)
+      call read_eos_table(eos_filename) !read in EOS table & set reference mass
+      call set_up_Hempel !set's up EOS for nuclear abundances
+
+      !!!!! The initialization of the weak rate library should be done through a seperate function call !!!!!
+      ! initialize weak-rate library if it is turned on in requested_interactions.inc
+      !if (add_nue_emission_weakinteraction_ecap.or.add_anue_emission_weakinteraction_poscap) then
+      !   call initialize_weakratelib(parameters_filename)
+      !endif
+
+      adhoc_nux_factor = 0.0d0 !increase for adhoc nux heating (also set
+      !add_nux_absorption_on_n_and_p to true)
+      !set up energies bins
+      do_integrated_BB_and_emissivity = .false.
+      mindx = 2.0d0
+      bin_bottom(1) = 0.0d0 !MeV
+      bin_bottom(2) = 2.0d0 !MeV
+      bin_bottom(3) = bin_bottom(2)+mindx
+      bin_bottom(number_groups) = 250.0d0
+
+
+      call nulib_series2(number_groups-1,bin_bottom(2),bin_bottom(number_groups),mindx,dxfac)
+      do i=4,number_groups
+         bin_bottom(i) = bin_bottom(i-1)+(bin_bottom(i-1)-bin_bottom(i-2))*dxfac
+      enddo
+
+      !calculate bin widths & energies from the bottom of the bin & energy at top on bin
+      do i=1,number_groups-1
+         energies(i) = (bin_bottom(i)+bin_bottom(i+1))/2.0d0
+         bin_widths(i) = bin_bottom(i+1)-bin_bottom(i)
+         bin_top(i) = bin_bottom(i+1)
+      enddo
+      energies(number_groups) = bin_bottom(number_groups)+bin_widths(number_groups-1)*dxfac/2.0d0
+      bin_widths(number_groups) = 2.0*(energies(number_groups)-bin_bottom(number_groups))
+      bin_top(number_groups) = bin_bottom(number_groups)+bin_widths(number_groups)
+
+    end subroutine standard_nulib_init
+
+
     subroutine example
       use nuclei_hempel
       use class_ratetable
@@ -50,4 +124,7 @@ module pynulib
       print *, "return_weakrate_from_table: ",log10(rate)
 
     end subroutine example
+
+
+
 end module pynulib
